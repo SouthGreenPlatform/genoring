76341ea3f456d83e148e64c8938d29de35af6d7a
Valentin Guignon <v.guignon@cgiar.org>
2025-04-15 09:31:12 +0000

    Removed JBrowse 2 module as it is not mature yet

--- modules/jbrowse2/hooks/disable_genoring.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Automatically exit on error.
-set -e
-
-# Remove JBrowse2 menu item.
-genoring remove_menuitem '/jbrowse2/'
-
--- modules/jbrowse2/hooks/enable_genoring.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Automatically exit on error.
-set -e
-
-# Add JBrowse2 menu item.
-genoring add_menuitem '/jbrowse2/' 'JBrowse2' 'main'
-
