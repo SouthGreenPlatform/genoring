66b0311b77382e385806f051585f4e47a00213b2
Valentin Guignon <guignonv@yahoo.fr>
2025-11-03 11:38:48 +0100

    Refactoring on module recipe removal

--- modules/brapimapper/hooks/uninstall_genoring.sh
+++ modules/brapimapper/hooks/uninstall_genoring.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 genoring uninstall_recipe modules/brapimapper/res/recipes/brapimapper_recipe
+# We uninstall BrAPI module as it is not supported by uninstall_recipe.
+genoring command drush -y pm-uninstall brapi
+genoring command composer --no-interaction remove drupal/brapi
